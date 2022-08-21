e59714588ce95cc89b96fe6ea375b438f2d7e1b6
sangeetha-hari <harikantrasangeetha4@gmail.com>
2022-08-21 21:04:58 +0530

    Second commit

--- src/Project.js
+++ src/Project.js
@@ -1,12 +1,41 @@
 import React from "react";
+import { Card, Button, CardTitle, CardText } from 'reactstrap';
+const projectlist=[
+  {
+   pid:1,
+    pname:"Graphics Project",
+    psummary:"An animation project using OpenGL"
+  },
+  {
+    pid:2,
+     pname:"IVRS for college",
+     psummary:"Built a IVR system for college and won Price in college level"
+   },
+   {
+    pid:3,
+     pname:"Opinion Mining",
+     psummary:"A Research project to analyze Twitter data on any productS"
+   }
+]
+
 export default function Project() {
   return (
-    <section>
-      project
-      <h4> Project in OpenGL</h4>
-      <h4>IVRS for college</h4>
-      <h4>Opinion Mining using twitter Data</h4>
-      <h4> </h4>
-    </section>
+    <div>
+      <h1> Project</h1>
+      <section className="flxcard">
+    
+    {projectlist.map((el) => {
+      return (
+        <Card className="styleprojecctcard">
+          <div >
+          <CardTitle className="medium">{el.pname}</CardTitle>
+          <CardText>{el.psummary}</CardText>
+    
+          </div>
+        </Card>
+      );
+    })}
+  </section>
+    </div>
   );
 }
