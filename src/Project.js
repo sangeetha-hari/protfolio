f1dbc0f01d758138ac3bffea9faa6895b884cc9c
sangeetha-hari <harikantrasangeetha4@gmail.com>
2023-05-20 21:32:54 +0530

    Updated project section

--- src/Project.js
+++ src/Project.js
@@ -1,61 +1,191 @@
 import React from "react";
-import { Card,CardTitle, CardText } from 'reactstrap';
-import KeyboardDoubleArrowDown from '@mui/icons-material/KeyboardDoubleArrowDown';
+import { Card, CardTitle, CardText,CardBody,CardSubtitle,Button } from "reactstrap";
+import KeyboardDoubleArrowDown from "@mui/icons-material/KeyboardDoubleArrowDown";
 import { Container } from "react-bootstrap";
-const projectlist=[
+import GitHubIcon from '@mui/icons-material/GitHub';
+// import {
+//   Carousel,
+//   CarouselItem,
+//   CarouselIndicators,
+// } from 'reactstrap';
+// import Card from '@mui/material/Card';
+// import CardActions from '@mui/material/CardActions';
+// import CardContent from '@mui/material/CardContent';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
+
+const projectlist = [
   {
-   pid:1,
-    pname:"Graphics Project",
-    psummary:"An animation project using OpenGL"
+    pid: 1,
+    pimg:"https://e1.pxfuel.com/desktop-wallpaper/243/6/desktop-wallpaper-mern-stack-bloggerboy-mern-stack-thumbnail.jpg",
+    pname: "Full Stack projects",
+    psummary: " SocialMedia Management tool",
+    gitlink1: "https://github.com/sangeetha-hari/sociapp_frontEnd.git",
+    gitlink2: "https://github.com/sangeetha-hari/SociBackendAPI.git",
+    deployed:""
   },
   {
-    pid:2,
-     pname:"IVRS for college",
-     psummary:"Built a IVR system for college and won Price in college level"
-   },
-   {
-    pid:3,
-     pname:"Opinion Mining",
-     psummary:"A Research project to analyze Twitter data on any product"
-   },
-   {
-    pid:4,
-     pname:"Full Stack projects",
-     psummary:"URL shortner, SocialMedia Management tool"
-   },
-   {
-    pid:5,
-     pname:"ReactJS Projects",
-     psummary:"Added item into cart"
-   },
-   {
-    pid:6,
-     pname:"NodeJS Projects",
-     psummary:"Student-Mentor APIs, Hall-booking API"
-   }
+    pid: 2,
+    pimg:"https://e1.pxfuel.com/desktop-wallpaper/243/6/desktop-wallpaper-mern-stack-bloggerboy-mern-stack-thumbnail.jpg",
+    pname: "Full Stack projects",
+    psummary: "Rental Application",
+    gitlink1: "https://github.com/sangeetha-hari/Rental_frontend.git",
+    gitlink2: "https://github.com/sangeetha-hari/Rental_Backend_API.git",
+  },
+  {
+    pid: 3,
+    pimg:"https://e1.pxfuel.com/desktop-wallpaper/243/6/desktop-wallpaper-mern-stack-bloggerboy-mern-stack-thumbnail.jpg",
+    pname: "Full Stack projects",
+    psummary: "Library system",
+    gitlink1: "https://github.com/sangeetha-hari/Rental_frontend.git",
+    gitlink2: "https://github.com/sangeetha-hari/Rental_Backend_API.git",
+  },
 
-]
+  {
+    pid: 3,
+    pimg:"https://s3.ap-southeast-1.amazonaws.com/arrowhitech.com/wp-content/uploads/2021/09/01031030/ReactJS.png",
+    pname: "ReactJS Projects",
+    psummary: "Added item into cart",
+    gitlink1: "https://github.com/sangeetha-hari/Rental_frontend.git",
+    
+  },
+  {
+    pid: 4,
+    pimg:"https://images.ctfassets.net/aq13lwl6616q/7cS8gBoWulxkWNWEm0FspJ/c7eb42dd82e27279307f8b9fc9b136fa/nodejs_cover_photo_smaller_size.png",
+    pname: "NodeJS Projects",
+    psummary: "Student-Mentor APIs, Hall-booking API",
+    gitlink1: "https://github.com/sangeetha-hari/Rental_frontend.git"
+  },
+    
+  {
+    pid: 5,
+    pimg:"https://contentstatic.techgig.com/photo/93000110/what-makes-java-still-popular-among-developers.jpg?28608",
+    pname: "Opinion Mining",
+    psummary: "A Research project to analyze Twitter data on any product",
+  },
+  {
+    pid: 6,
+    pimg:"https://i.stack.imgur.com/NZ0Wp.png",
+    pname: "Graphics Project",
+    psummary: "An animation project using OpenGL",
+  },
+  {
+    pid: 7,
+    pimg:"https://wakeupandcode.com/wp-content/uploads/2019/03/C.NET_-1024x384-1.png",
+    pname: "IVRS for college",
+    psummary: "Built a IVR system for college and won Price in college level",
+  }
+];
+
+
 
 export default function Project() {
+
+  
+  
+
   return (
-    <Container>
-      <br/><br/><br/>
-      <h1 className="largestyle"><b style={{color:"white"}}>Projects</b></h1>
-      <section className="flxcard">
+   <div>
+      <Container className="header">
+        <br/>
+        <br/>
+        <br/>
+        <h1 className="largestyle">
+          <b style={{ color: "white" }}>Projects</b>
+        </h1>
+       
+        <div className="project">
+          {projectlist.map((item)=>{
+            return(
+<Card style={{ width: '18rem', marginTop:"2rem"}}>
+        <img height={100}
+    alt="Sample"
+    src={item.pimg}
+  />
+  <CardBody>
+    <CardTitle tag="h5">
+      Card title
+    </CardTitle>
+    <CardSubtitle
+      className="mb-2 text-muted"
+      tag="h6"
+    >
+    {item.pname}
+    </CardSubtitle>
+    <CardText style={{ color: "black" }} >
+     {item.psummary} 
+    </CardText>
+    {item.pname==="Full Stack projects"?<p><Button>
+      <a href={item.gitlink1} style={{ color: "black"}}>FrontEnd <GitHubIcon/></a> 
+      </Button>" "
+      <Button>
+      <a href={item.gitlink2} style={{ color: "black"}}>BackEnd <GitHubIcon/></a> 
+      </Button>
+      </p>: 
+      item.gitlink1?<Button>
+      <a href={item.gitlink1} style={{ color: "black"}}> <GitHubIcon/></a> 
+      </Button>: <></>}
     
-    {projectlist.map((el) => {
-      return (
-        <Card className="styleprojecctcard">
-          <div >
-          <CardTitle className="medium">{el.pname}</CardTitle>
-          <CardText>{el.psummary}</CardText>
-    
-          </div>
+  </CardBody>
         </Card>
-      );
-    })}
-  </section>
-  <a  href="#contact"><KeyboardDoubleArrowDown fontSize='large'/></a>
-    </Container>
+            )
+          })}
+        
+        {/* <Card style={{ width: '18rem', marginTop:"2rem"}}>
+        <img
+    alt="Sample"
+    src="https://picsum.photos/300/200"
+  />
+        </Card>
+        <Card style={{ width: '18rem', marginTop:"2rem"}}>
+        <img
+    alt="Sample"
+    src="https://picsum.photos/300/200"
+  />
+        </Card>
+        <Card style={{ width: '18rem', marginTop:"2rem"}}>
+        <img
+    alt="Sample"
+    src="https://picsum.photos/300/200"
+  />
+        </Card>
+        <Card style={{ width: '18rem', marginTop:"2rem"}}>
+        <img
+    alt="Sample"
+    src="https://picsum.photos/300/200"
+  />
+        </Card> */}
+        </div>
+        {/* <section className="flxcard">
+          {projectlist.map((el) => {
+            return (
+              <Card className="styleprojecctcard">
+                <div>
+                  <CardTitle className="small">{el.pname}</CardTitle>
+                  <CardText>{el.psummary}</CardText>
+
+                  {el.gitlink1 ? (
+                    <div>
+                      {" "}
+                      <a href={el.gitlink1}>FrontEnd Git Repo</a>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {el.gitlink2 ? (
+                    <a href={el.gitlink1}>BackEnd Git Repo</a>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              </Card>
+            );
+          })}
+        </section> */}
+        <a href="#contact">
+          <KeyboardDoubleArrowDown fontSize="large" />
+        </a>
+      </Container>
+      </div>
   );
 }
